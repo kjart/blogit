00b7fcceb7d04bbae579202878f8f5852bb5e7f0
hackthievist <sobogunifeoluwa@gmail.com>
2019-04-06 17:52:58 +0100

    Updated index.php for readability

--- post.php
+++ post.php
@@ -8,23 +8,28 @@ $converter->getConfig()->setOption('italic_style', '*');
 $converter->getConfig()->setOption('bold_style', '__');
 $converter->getConfig()->setOption('hard_break', true);
 
-
-if(isset($_POST['submit'])){
+if (isset($_POST['submit'])) {
     $title = $_POST['title'];
     $author = $_POST['author'];
     $post = $_POST['post'];
     date_default_timezone_set('Africa/Lagos');
     $date = date('m-d-Y', time());
-
+    $time = time();
 
     try {
         //convert html to markdown
-        $markdown = $converter->convert($post);
+        $markdownAuthor = $converter->convert($author);
+        $markdownTitle = $converter->convert($title);
+        $markdownPost = $converter->convert($post);
 
+        $markdown = ["Author" => $markdownAuthor, "Title" => $markdownTitle, "Post" => $markdownPost, "Date" => $date, "Time" => $time];
+        $redactedTitle = strtolower(preg_replace('/\s+/', '-', $title));
         //add the directory and file name
-        $filename = '.' . DIRECTORY_SEPARATOR . "posts" . DIRECTORY_SEPARATOR . $date . '.md';
+        $filename = '.' . DIRECTORY_SEPARATOR . "posts" . DIRECTORY_SEPARATOR . $redactedTitle . "-" . $date . "-" . $time . '.md';
         //php native function to write to the file
-        $handle = file_put_contents($filename, $markdown);
+        foreach ($markdown as $key => $value) {
+            $handle = file_put_contents($filename, $key . ": " . $value . "\r\n", FILE_APPEND);
+        }
         echo 'Post saved!';
     } catch (\Throwable $th) {
 
@@ -33,5 +38,3 @@ if(isset($_POST['submit'])){
     }
 
 }
-
-?>
--- posts/04-06-2019.md
+++
@@ -1,8 +0,0 @@
-This is a new post
-
-- Post 1
-- post 2
-- post 3
-- post 4
- 
-[ceofred](http://localhost/ceofred.io)
